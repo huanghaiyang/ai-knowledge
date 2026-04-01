2f411c60ed1e502d7acba283337587083ff1b924
huanghaiyang <huanhai24@sina.com>
2026-04-01 16:45:27 +0800

    pwd encry

--- backend/app/utils/common.py
+++ backend/app/utils/common.py
@@ -1,11 +1,11 @@
 from passlib.context import CryptContext
 
-# 密码加密上下文，使用内置的pbkdf2_sha256算法
-pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
+# 密码加密上下文，使用更安全的argon2算法
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 def get_password_hash(password: str) -> str:
-    # bcrypt限制密码长度为72字节，超过会被截断
-    password = password[:72]
+    # argon2限制密码长度为4096字节，超过会被截断
+    password = password[:4096]
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
